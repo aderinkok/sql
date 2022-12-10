dad84a8cde4e7cb02860107d8eb6fb1218c2570f
aderinkok <abdulmelik93@msn.com>
2022-12-10 17:58:41 +0300

    Update tarih.sql

--- fonksiyonlar/tarih.sql
+++ fonksiyonlar/tarih.sql
@@ -1,13 +1,15 @@
 -- Tarih Fonksiyonlarý
 --syntax
 
-GETDATE([SAYI],[US])
-CURRENT_TIMESTAMP
-SYSDATETIME
-DATEADD([TUR],[SAYI],[TARÝH])
--- [TUR] parametresi MILLISECOND,SECOND,MINUTE,HOUR, DAY,WEEK,MONTH ya da YEAR olabilir.
-DATEDIFF([TUR],[TARÝH1],[TARÝH2])
--- [TUR] parametresi DAY,MONTH ya da YEAR olabilir.
+ GETDATE()
+ CURRENT_TIMESTAMP
+ SYSDATETIME()
+ DATEADD([TUR],[SAYI],[TARÝH])
+ -- [TUR] parametresi MILLISECOND, SECOND, MINUTE, HOUR, DAY, WEEK, MONTH ya da YEAR olabilir. 
+ DATEDIFF([TUR],[TARÝH1],[TARÝH2])
+ -- [TUR] parametresi MILLISECOND, SECOND, MINUTE, HOUR, DAY, WEEK, MONTH ya da YEAR olabilir.
+ DATEPART([TUR],[TARÝH])
+ -- [TUR] parametresi MILLISECOND, SECOND, MINUTE, HOUR, DAY, WEEK, MONTH ya da YEAR olabilir.
 
  --Örnek - GETDATE 
 
@@ -20,12 +22,39 @@ SELECT GETDATE()
 
 Komutun çalýþtýrýldýðý andaki tarih bilgisini YYYY-AA-GG SS:DD:ss.xxx formatýnda yazdýrýr.
 */
+
+ --Örnek - CURRENT_TIMESTAMP 
+
+SELECT CURRENT_TIMESTAMP
+
+
+-- Örnek - CURRENT_TIMESTAMP Çýktý
+/*
+|(No column name)         |
+| YYYY-AA-GG SS:DD:ss.xxx | 
+
+Komutun çalýþtýrýldýðý andaki tarih bilgisini YYYY-AA-GG SS:DD:ss.xxx formatýnda yazdýrýr.
+*/
+
+ --Örnek -  SYSDATETIME 
+
+SELECT  SYSDATETIME()
+
+
+-- Örnek -  SYSDATETIME Çýktý
+/*
+|(No column name)         |
+| YYYY-AA-GG SS:DD:ss.xxxxxxx | 
+
+Komutun çalýþtýrýldýðý andaki tarih bilgisini YYYY-AA-GG SS:DD:ss.xxx formatýnda yazdýrýr.
+*/
+
  --Örnek - DATEADD 
 
 SELECT DATEADD(YEAR,5,'2022-12-03')  --1
 SELECT DATEADD(MONTH,5,'2022-12-03') --2
 SELECT DATEADD(DAY,5,'2022-12-03')   --3
-
+SELECT DATEADD(MONTH,-5,'2022-01-01') --4
 -- Örnek - DATEADD Çýktý
 /*
 |(No column name)         | [1]
@@ -36,6 +65,9 @@ SELECT DATEADD(DAY,5,'2022-12-03')   --3
 
 |(No column name)         | [3]
 | 2022-12-08 00:00:00.000 | 
+
+|(No column name)         | [4]
+| 2021-08-01 00:00:00.000 | 
 
 */
 
@@ -58,13 +90,13 @@ SELECT DATEDIFF(DAY,'2019-05-08','2022-12-03')   --3
 
 */
 
- --Örnek - DATEDIFF 
+ --Örnek - DATEPART 
 
 SELECT DATEPART(YEAR,'2022-12-03')    --1
 SELECT DATEPART(MONTH,'2022-12-03') --2
 SELECT DATEPART(DAY,'2022-12-03')   --3
 
--- Örnek - DATEDIFF Çýktý
+-- Örnek - DATEPART Çýktý
 /*
 |(No column name) | [1]
 | 2022            | 
@@ -77,5 +109,3 @@ SELECT DATEPART(DAY,'2022-12-03')   --3
 
 */
 
-
-https://medium.com/machine-learning-t%C3%BCrkiye/sql-server-tarih-fonksiyonlar%C4%B1-d4cce45186b7
